6c36271d2cfae5b8e5ad8e4901e2d33341a1dd40
cindy <cindy@10.0.0.96>
2024-01-17 09:13:35 -0700

    Initial Commit

---
+++ src/demo/TestDemo.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class TestDemo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Day GitHub 2...");
+		
+		System.out.println("Exercise1...");
+		
+	}
+	
+
+}
